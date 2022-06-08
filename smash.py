7a57f826d3b0debcb0d0ce9bebabd876d7fc5552
Matt <m.f.grace22@gmail.com>
2022-06-08 13:27:54 -0400

    8 kyu code wars exercise `smash`

---
+++ smash.py
@@ -0,0 +1,8 @@
+'''
+smash
+    * Should return empty string for empty array.
+    * One word example should return the word.
+    * Multiple words should be separated by spaces.
+'''
+def smash(words):
+    return " ".join(words)
